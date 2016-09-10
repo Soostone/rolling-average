939dfdfde84c4cdb4e292458ade43be80240ec18
Michael Xavier <michael@michaelxavier.net>
2016-09-09 17:22:28 -0700

    Clean up bench warnings

--- bench/Main.hs
+++ bench/Main.hs
@@ -16,17 +16,22 @@ main :: IO ()
 main = defaultMain [
   bgroup "ravgAdd" $ flip map [1, 100, 1000] $ \n ->
     bgroup (show n) [
-        bench "Int" $ whnf (foldl' ravgAdd (mkRavg n)) ([1..fromIntegral n] :: [Int])
-      , bench "Integer" $ whnf (foldl' ravgAdd (mkRavg n)) ([1..fromIntegral n] :: [Integer])
+        bench "Int" $ whnf (avgList n) ([1..fromIntegral n] :: [Int])
+      , bench "Integer" $ whnf (avgList n) ([1..fromIntegral n] :: [Integer])
       ]
   , bgroup "ravg" $ flip map [1, 100, 1000] $ \n ->
       bgroup (show n) [
-          bench "Int" $ whnf ravg (foldl' ravgAdd (mkRavg n) ([1..fromIntegral n] :: [Int]))
-        , bench "Integer" $ whnf ravg (foldl' ravgAdd (mkRavg n) ([1..fromIntegral n] :: [Integer]))
+          bench "Int" $ whnf ravg' (avgList n ([1..fromIntegral n] :: [Int]))
+        , bench "Integer" $ whnf ravg' (avgList n ([1..fromIntegral n] :: [Integer]))
       ]
   ]
 
 
 -------------------------------------------------------------------------------
 avgList :: Num a => Int -> [a] -> RollingAvg a
-avgList lim samples = foldl' ravgAdd (mkRavg lim) samples
+avgList lim = foldl' ravgAdd (mkRavg lim)
+
+
+-------------------------------------------------------------------------------
+ravg' :: (Integral a) => RollingAvg a -> Double
+ravg' = ravg
