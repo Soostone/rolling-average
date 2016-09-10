f1d17cf1d8c893aaf78acaf75fab7ad65244d28e
Michael Xavier <michael@michaelxavier.net>
2016-09-09 17:09:49 -0700

    Add Werror

--- test/Main.hs
+++ test/Main.hs
@@ -7,6 +7,7 @@ module Main
 -------------------------------------------------------------------------------
 import           Data.Foldable             as FT
 import           Data.Ratio
+import           Data.Sequence             as Seq
 import           Test.Tasty
 import           Test.Tasty.HUnit
 import           Test.Tasty.QuickCheck
@@ -23,14 +24,14 @@ tests :: TestTree
 tests = testGroup "rolling-average"
   [
     testProperty "window size of 0 always produces 0" $ \(samples :: [Int]) ->
-      ravg (avgList 0 samples) === 0.0
+      ravg (avgList 0 samples) === (0.0 :: Double)
   , testProperty "never grows samples larger than the limit" $ \(Positive lim) (samples :: [Int]) ->
-      FT.length (ravgSamples (avgList lim samples)) <= lim
+      Seq.length (ravgSamples (avgList lim samples)) <= lim
   , testCase "can calculate rolling average as a Ratio" $ do
-      ravg (avgList 3 [0, 1, 2, 3, 4, 5]) @?= (3 + 4 + 5) % 3
+      ravg (avgList 3 ([0, 1, 2, 3, 4, 5] :: [Int])) @?= (((3 + 4 + 5) % 3) :: Rational)
   ]
 
 
 -------------------------------------------------------------------------------
 avgList :: Num a => Int -> [a] -> RollingAvg a
-avgList lim samples = foldl' ravgAdd (mkRavg lim) samples
+avgList lim samples = FT.foldl' ravgAdd (mkRavg lim) samples
